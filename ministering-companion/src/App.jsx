b1cc09779e27fadcb1db396415412b70dfe8880c
Your GitHub Username <angelangelis314@gmail.com>
2025-10-12 20:03:25 -0700

    Initial commit

---
+++ ministering-companion/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ padding: "2rem", fontFamily: "sans-serif" }}>
+      <h1>Ministering Companionship App</h1>
+      <p>Welcome! This is the beginning of something spiritually beautiful.</p>
+    </div>
+  );
+}
+
+export default App;
